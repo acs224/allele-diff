e2cd96499efb45259a124b05f36af2fedf065c78
Andrew Shinohara <acs224@georgetown.edu>
2013-05-03 17:35:06 -0400

    Renaming things to haplotypes instead of alleles to be more descriptive. Also commenting code

--- allele_diff.py
+++ allele_diff.py
@@ -5,75 +5,134 @@ from collections import defaultdict
 from operator import itemgetter
 
 class AlleleDiff:
-  def __init__(self, allele_file="HLAB.txt"):
-    self.alleles = self.parse_alleles(allele_file)
+  """Object to facilitate the building of a haplotype translation file."""
+    
+  def __init__(self, haplo_file="HLAB.txt"):
+    """Construct an AlleleDiff object.
+    
+    Kwargs: 
+      haplo_file: File to use for haplotype definitions.
+      
+    """
+    # Number of header rows in the haplotype files
+    self.HEADER_ROWS = 3
+    # Special characters in haplotype file
+    self.EXON = '|'
+    self.NO_CALL = '*'
+    
+    self.haplos = self.parse_haplos(haplo_file)
 
-  def parse_alleles(self, allele_file):
-    allele_definitions = {}
-    with open(allele_file) as f:
+  def parse_haplos(self, haplo_file):
+    """Parse the haplo_file into a list of POS:BASE combinations.
+    
+    Args:
+      haplo_file: File to parse
+      
+    Returns a dict where haplotype is the key and the value is the list of SNP definitions for that haplotype.
+      
+    """
+    haplo_definitions = {}
+    with open(haplo_file) as f:
       for i, line in enumerate(f):
-        if i < 3:
+        if i < self.HEADER_ROWS:
           continue
         allele_def = line.split()
         allele_string = allele_def[1:]
-        allele_definitions[allele_def[0]] = ["%s:%s" % (i, base) for i, base in enumerate(allele_string) if ('|' != base or '*' != base)]
-    return allele_definitions
+        # Ignore positions that are NO_CALL or EXON definitions.  
+        # NO_CALL can be ignored since it does not contain any basepair definition.
+        haplo_definitions[allele_def[0]] = ["%s:%s" % (i, base) for i, base in enumerate(allele_string) if (self.EXON != base or self.NO_CALL != base)]
+    return haplo_definitions
  
-  def check_alleles(self, important, unimportant, snp_filter):
-    logging.debug("In check_alleles.  Important: %s Filter: %s" % (important, snp_filter))
-    important_alleles = self.filter_alleles(important, snp_filter)
-    unimportant_alleles = self.filter_alleles(unimportant, snp_filter, True)
-    remaining_important = [allele for allele in important if allele not in important_alleles]
-    filtered_snps = [temp_snp['snp'] for temp_snp in snp_filter]
-    if not important or len(important_alleles) == 0:
-      return []
-    logging.debug("Before recursion check.  snp_filter: %s important %s unimportant %s" % (snp_filter, important_alleles, unimportant_alleles))
-    if len(snp_filter) > 0 and len(important_alleles) == 1 and len(unimportant_alleles) == 0:
-      logging.debug("About to recurse.  important: %s remaining_important %s" % (important_alleles[0], remaining_important))
-      return [{'allele': important_alleles[0], 'snps': filtered_snps}] + self.check_alleles(remaining_important, unimportant, [])
+  def check_haplos(self, important, unimportant, snp_filter):
+    """Recursively builds a haplotype translation table based upon the important and unimportant haplotypes.
+    
+    Args:
+      important: List of important haplotypes.
+      unimportant: List of unimportant haplotypes.
+      snp_filter: List of snps to use in filtering haplotypes.
       
-    all_alleles = important_alleles + unimportant_alleles
+    Returns a list of dictonaries, one for each important haplotype.  Each dictonary is formatted:
+    {haplotype: haplotype name, snps: snps to translate that haplotype}
+    
+    """
+    logging.debug("In check_haplos.  Important: %s Filter: %s" % (important, snp_filter))
+    important_haplos = self.filter_haplos(important, snp_filter)
+    unimportant_haplos = self.filter_haplos(unimportant, snp_filter, True)
+    remaining_important = [haplo for haplo in important if haplo not in important_haplos]
+    filtered_snps = [temp_snp['snp'] for temp_snp in snp_filter]
+    if not important or len(important_haplos) == 0:
+      return []
+    logging.debug("Before recursion check.  snp_filter: %s important %s unimportant %s" % (snp_filter, important_haplos, unimportant_haplos))
+    if len(snp_filter) > 0 and len(important_haplos) == 1 and len(unimportant_haplos) == 0:
+      logging.debug("About to recurse.  important: %s remaining_important %s" % (important_haplos[0], remaining_important))
+      return [{'haplotype': important_haplos[0], 'snps': filtered_snps}] + self.check_haplos(remaining_important, unimportant, [])
+      
+    all_haplos = important_haplos + unimportant_haplos
     snp_map = defaultdict(lambda: defaultdict(list))
-    for allele in all_alleles:
-      seq = self.alleles[allele]
+    for haplo in all_haplos:
+      seq = self.haplos[haplo]
       for item in seq:
-        snp_map[item]['present'].append(allele)
+        snp_map[item]['present'].append(haplo)
         
-    all_snp_scores = self.score_snps(important_alleles, unimportant_alleles, snp_map)
+    all_snp_scores = self.score_snps(important_haplos, unimportant_haplos, snp_map)
     for to_test in all_snp_scores:
       if to_test['snp'] not in filtered_snps:
         snp_filter.append(to_test)
-        return self.check_alleles(important, unimportant, snp_filter)
+        return self.check_haplos(important, unimportant, snp_filter)
     return []
   
-  def filter_alleles(self, alleles, snp_filter, include=True):
-    if len(snp_filter) < 1:
-      return alleles
-    to_filter = [snp['snp'] for snp in snp_filter]
-    filtered_alleles = []
-    if not include:
-      filtered_alleles = list(alleles)
-    for allele in alleles:
-      seq = self.alleles[allele]
-      if include and all([snp in seq for snp in to_filter]):
-        filtered_alleles.append(allele)
-      elif not include and not any([snp in seq for snp in to_filter]):
-        filtered_alleles.remove(allele)
-
-    return filtered_alleles
+  def filter_haplos(self, haplos, snp_filter, include=True):
+    """Filters haplotypes out based on if they have certain snps or not.
     
-  def score_snps(self, important_alleles, unimportant_alleles, snp_map):
-    all_snp_scores = []
-    for snp, allele_map in snp_map.iteritems():
+    Args:
+      haplos: List of haplotypes to filter
+      snp_filter: List of snps to use as filter criteria
       
-      important_snp_allele = [allele for allele in allele_map['present'] if allele in important_alleles]
-      unimportant_snp_allele = [allele for allele in  allele_map['present'] if allele in unimportant_alleles]
-      important_ratio = len(important_snp_allele) / float(len(important_alleles))
+    Kwargs:
+      include:  True if criteria should be used to include haplotypes, False if it should
+                exclude haplotypes
+      
+    Returns a list of haplotypes that have been filtered based on criteria
+    
+    """
+    if len(snp_filter) < 1:
+      return haplos
+    to_filter = [snp['snp'] for snp in snp_filter]
+    filtered_haplos = []
+    if not include:
+      filtered_haplos = list(haplos)
+    for haplo in haplos:
+      seq = self.haplos[haplo]
+      if include and all([snp in seq for snp in to_filter]):
+        filtered_haplos.append(haplo)
+      elif not include and not any([snp in seq for snp in to_filter]):
+        filtered_haplos.remove(haplo)
+
+    return filtered_haplos
+    
+  def score_snps(self, important_haplos, unimportant_haplos, snp_map):
+    """Assign a score to each snp.  This score is meant to minimize the number
+    of snps that should be used to define a haplotype.  The current formula is:
+    
+    score = (ratio of snp in important haplos) - (ratio of snp in unimportant haplos) + (weighted score to favor snps over reference)
+    
+    Args:
+      important_haplos: List of important haplotypes.
+      unimportant_haplos: List of unimportant haplotypes.
+      snp_map: Dictionary of all snps.
+      
+    Returns a list of scored snps. (Higher score is better, has more chance of having useful information)
+    """
+    all_snp_scores = []
+    for snp, haplo_map in snp_map.iteritems():
+      
+      important_snp_haplos = [hap for hap in haplo_map['present'] if hap in important_haplos]
+      unimportant_snp_haplos = [hap for hap in  haplo_map['present'] if hap in unimportant_haplos]
+      important_ratio = len(important_snp_haplos) / float(len(important_haplos))
       unimportant_ratio = 0
-      if len(unimportant_alleles) > 0:
-        unimportant_ratio = len(unimportant_snp_allele) / float(len(unimportant_alleles))
+      if len(unimportant_haplos) > 0:
+        unimportant_ratio = len(unimportant_snp_haplos) / float(len(unimportant_haplos))
       if important_ratio > 0:
-        
         raw_score = important_ratio - unimportant_ratio + (0 if "_" in snp else 1)
         all_snp_scores.append({'snp': snp, 'score': raw_score})
         
@@ -83,6 +142,6 @@ if __name__ == "__main__":
   
   allele_diff = AlleleDiff()
   important_alleles = ['B*51240301', 'B*55070101']
-  unimportant_alleles = [allele for allele in allele_diff.alleles.keys() if allele not in important_alleles]
-  allele_mapping = allele_diff.check_alleles(important_alleles, unimportant_alleles, [])
+  unimportant_alleles = [allele for allele in allele_diff.haplos.keys() if allele not in important_alleles]
+  allele_mapping = allele_diff.check_haplos(important_alleles, unimportant_alleles, [])
   print allele_mapping
--- test_allele_diff.py
+++ test_allele_diff.py
@@ -6,81 +6,81 @@ class TestAlleleDiff(unittest.TestCase):
   
   def setUp(self):
     #logging.basicConfig(level=logging.DEBUG)
-    self.allele_diff = AlleleDiff("test_alleles.txt")
-    self.all_allele_diff = AlleleDiff("HLAB.txt")
+    self.haplo_diff = AlleleDiff("test_haplos.txt")
+    self.all_haplo_diff = AlleleDiff("HLAB.txt")
     
-  # def test_filter_alleles_include(self):
-  #   filtered = self.allele_diff.filter_alleles(["B*51240301"], [{'snp': '0:A'}])
-  #   self.assertEqual(1, len(filtered))
-  #   
-  #   filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '0:A'}])
-  #   self.assertEqual(2, len(filtered))
-  #   
-  # def test_filter_alleles_2_include(self):
-  #   filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '0:A'}, {'snp': '2:C'}])
-  #   self.assertEqual(1, len(filtered))
-  #   
-  # def test_filter_alleles(self):
-  #   filtered = self.allele_diff.filter_alleles(["B*51240301"], [{'snp': '0:A'}])
-  #   self.assertEqual(1, len(filtered))
-  #   
-  #   filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '0:A'}])
-  #   self.assertEqual(2, len(filtered))
-  #   
-  #   filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '11:G'}])
-  #   self.assertEqual(2, len(filtered))
-  #   
-  # def test_filter_alleles_2_exclude(self):
-  #   filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '0:A'}, {'snp': '2:C'}], False)
-  #   self.assertEqual(4, len(filtered))
-  #   
-  # def test_check_1_allele(self):
-  #    important_alleles = ['B*55070101']
-  #    unimportant_alleles = [allele for allele in self.allele_diff.alleles.keys() if allele not in important_alleles]
-  #    checked = self.allele_diff.check_alleles(important_alleles, unimportant_alleles, [])
-  #    self.assertEqual(2, len(checked[0]['snps']))
-  #   
-  # def test_check_2_alleles(self):
-  #   important_alleles = ['B*55070101', 'B*07420101']
-  #   unimportant_alleles = [allele for allele in self.allele_diff.alleles.keys() if allele not in important_alleles]
-  #   checked = self.allele_diff.check_alleles(important_alleles, unimportant_alleles, [])
-  #   self.assertEqual(2, len(checked))
-  #   
-  # def test_check_4_alleles(self):
-  #   important_alleles = ['B*51240301', 'B*55070101', 'B*07420101', 'B*27090101']
-  #   unimportant_alleles = [allele for allele in self.allele_diff.alleles.keys() if allele not in important_alleles]
-  #   checked = self.allele_diff.check_alleles(important_alleles, unimportant_alleles, [])
-  #   self.assertEqual(4, len(checked))    
-  #   
-  # def test_all_check_1(self):
-  #   important_alleles = ['B*55070101']
-  #   #important_alleles = ['B*51240301']
-  #   unimportant_alleles = [allele for allele in self.all_allele_diff.alleles.keys() if allele not in important_alleles]
-  #   checked = self.all_allele_diff.check_alleles(important_alleles, unimportant_alleles, [])
-  #   self.assertEqual(1, len(checked))   
+  def test_filter_haplos_include(self):
+    filtered = self.haplo_diff.filter_haplos(["B*51240301"], [{'snp': '0:A'}])
+    self.assertEqual(1, len(filtered))
+    
+    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '0:A'}])
+    self.assertEqual(2, len(filtered))
+    
+  def test_filter_haplos_2_include(self):
+    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '0:A'}, {'snp': '2:C'}])
+    self.assertEqual(1, len(filtered))
+    
+  def test_filter_haplos(self):
+    filtered = self.haplo_diff.filter_haplos(["B*51240301"], [{'snp': '0:A'}])
+    self.assertEqual(1, len(filtered))
+    
+    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '0:A'}])
+    self.assertEqual(2, len(filtered))
+    
+    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '11:G'}])
+    self.assertEqual(2, len(filtered))
+    
+  def test_filter_haplos_2_exclude(self):
+    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '0:A'}, {'snp': '2:C'}], False)
+    self.assertEqual(4, len(filtered))
+    
+  def test_check_1_allele(self):
+     important_haplos = ['B*55070101']
+     unimportant_haplos = [allele for allele in self.haplo_diff.haplos.keys() if allele not in important_haplos]
+     checked = self.haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+     self.assertEqual(2, len(checked[0]['snps']))
+    
+  def test_check_2_haplos(self):
+    important_haplos = ['B*55070101', 'B*07420101']
+    unimportant_haplos = [allele for allele in self.haplo_diff.haplos.keys() if allele not in important_haplos]
+    checked = self.haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+    self.assertEqual(2, len(checked))
+    
+  def test_check_4_haplos(self):
+    important_haplos = ['B*51240301', 'B*55070101', 'B*07420101', 'B*27090101']
+    unimportant_haplos = [allele for allele in self.haplo_diff.haplos.keys() if allele not in important_haplos]
+    checked = self.haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+    self.assertEqual(4, len(checked))    
+    
+  def test_all_check_1(self):
+    important_haplos = ['B*55070101']
+    #important_haplos = ['B*51240301']
+    unimportant_haplos = [allele for allele in self.all_haplo_diff.haplos.keys() if allele not in important_haplos]
+    checked = self.all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+    self.assertEqual(1, len(checked))   
         
-  # def test_all_check_4(self):
-  #   important_alleles = ['B*51240301', 'B*55070101', 'B*07420101', 'B*27090101']
-  #   unimportant_alleles = [allele for allele in self.all_allele_diff.alleles.keys() if allele not in important_alleles]
-  #   checked = self.all_allele_diff.check_alleles(important_alleles, unimportant_alleles, [])
-  #   self.assertEqual(4, len(checked))
+  def test_all_check_4(self):
+    important_haplos = ['B*51240301', 'B*55070101', 'B*07420101', 'B*27090101']
+    unimportant_haplos = [allele for allele in self.all_haplo_diff.haplos.keys() if allele not in important_haplos]
+    checked = self.all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+    self.assertEqual(4, len(checked))
   
-  def test_all_check_10(self):
-    important_alleles = ['B*51240301', 'B*55070101', 'B*35700101', 'B*07420101', 'B*27090101', 'B*51560101', 'B*07760101', 'B*50060101', 'B*08340101', 'B*52060201']
-    unimportant_alleles = [allele for allele in self.all_allele_diff.alleles.keys() if allele not in important_alleles]
-    checked = self.all_allele_diff.check_alleles(important_alleles, unimportant_alleles, [])
-    self.assertEqual(10, len(checked))
+  # def test_all_check_10(self):
+  #   important_haplos = ['B*51240301', 'B*55070101', 'B*35700101', 'B*07420101', 'B*27090101', 'B*51560101', 'B*07760101', 'B*50060101', 'B*08340101', 'B*52060201']
+  #   unimportant_haplos = [allele for allele in self.all_haplo_diff.haplos.keys() if allele not in important_haplos]
+  #   checked = self.all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+  #   self.assertEqual(10, len(checked))
     
   # def test_all_check(self):
-  #   for important_allele in self.all_allele_diff.alleles.keys():
-  #     important_alleles = [important_allele]
-  #     unimportant_alleles = [allele for allele in self.all_allele_diff.alleles.keys() if allele not in important_alleles]
-  #     checked = self.all_allele_diff.check_alleles(important_alleles, unimportant_alleles, [])
+  #   for important_allele in self.all_haplo_diff.haplos.keys():
+  #     important_haplos = [important_allele]
+  #     unimportant_haplos = [allele for allele in self.all_haplo_diff.haplos.keys() if allele not in important_haplos]
+  #     checked = self.all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
   #     print checked
   #     if len(checked) != 1:
-  #       print "ERROR", important_alleles, checked
+  #       print "ERROR", important_haplos, checked
   #     else:
-  #       print "no error", important_alleles
+  #       print "no error", important_haplos
     
 if __name__ == '__main__':
     unittest.main()
