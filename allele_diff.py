25afd8bc4529a3b65563d5980e10d0203da9942e
Andrew Shinohara <acs224@georgetown.edu>
2013-05-03 09:46:19 -0400

    Code is working for small test cases

--- allele_diff.py
+++ allele_diff.py
@@ -1,98 +1,40 @@
 import string
 import operator
+import logging
 from collections import defaultdict
-from itertools import combinations
 from operator import itemgetter
 
 class AlleleDiff:
-  def __init__(self, allele_file="HLAB.txt", wildtype="B*15020101"):
-    self.alleles = self.parse_alleles(allele_file, wildtype)
+  def __init__(self, allele_file="HLAB.txt"):
+    self.alleles = self.parse_alleles(allele_file)
+
+  def parse_alleles(self, allele_file):
+    allele_definitions = {}
+    with open(allele_file) as f:
+      for i, line in enumerate(f):
+        if i < 3:
+          continue
+        allele_def = line.split()
+        allele_string = allele_def[1:]
+        allele_definitions[allele_def[0]] = ["%s:%s" % (i, base) for i, base in enumerate(allele_string) if '_' != base and '|' != base]
+    return allele_definitions
  
-  def filter_alleles(self, alleles, snp_filter, include=True):
-    if len(snp_filter) < 1:
-      return alleles
-    filtered_alleles = []
-    if not include:
-      filtered_alleles = alleles
-    for allele in alleles:
-      seq = self.alleles[allele]
-      to_filter = [snp['snp'] for snp in snp_filter]
-      if include and all([snp in seq for snp in to_filter]):
-        filtered_alleles.append(allele)
-      elif not include and any([snp in seq for snp in to_filter]):
-        filtered_alleles.remove(allele)
-        
-    return filtered_alleles
+  def check_alleles(self, important, unimportant, snp_filter=[]):
     
-  # def check_alleles(self, important, unimportant, snp_filter=[]):
-  #   
-  #   print important, snp_filter
-  #   important_alleles = self.filter_alleles(important, snp_filter)
-  #   remaining_important = [allele for allele in important if allele not in important_alleles]
-  #   if not important or not important_alleles:
-  #     return []
-  #   filtered_snps = [temp_snp['snp'] for temp_snp in snp_filter]
-  #   
-  #   print important_alleles, remaining_important
-  #   unimportant_alleles = self.filter_alleles(unimportant, snp_filter, False)
-  #   if len(snp_filter) > 1 and len(important_alleles) == 1:
-  #     return [{'allele': important_alleles[0], 'snps': filtered_snps}] + self.check_alleles(remaining_important, unimportant)
-  #     
-  #   #print "FILTER:", len(important), snp_filter
-  #   
-  #   all_alleles = important_alleles + unimportant_alleles
-  #   #print "IMPORTANT:", len(important_alleles), len(unimportant_alleles), len(all_alleles)
-  #   num_important = len(important_alleles)
-  #   num_unimportant = len(unimportant_alleles)
-  #   snp_map = defaultdict(lambda: defaultdict(list))
-  #   for allele in all_alleles:
-  #     seq = self.alleles[allele]
-  #     for item in seq:
-  #       snp_map[item]['present'].append(allele)
-  #       
-  #   max_snp = None
-  #   for snp, allele_map in snp_map.iteritems():
-  #     important_snp_allele = [allele for allele in allele_map['present'] if allele in important_alleles]
-  #     unimportant_snp_allele = [allele for allele in  allele_map['present'] if allele in unimportant_alleles]
-  #     #print num_important, num_unimportant
-  #     important_ratio = len(important_snp_allele) / float(num_important)
-  #     unimportant_ratio = len(unimportant_snp_allele) / float(num_unimportant)
-  #     raw_score = important_ratio - unimportant_ratio
-  #     #print snp, raw_score
-  #     #print snp, len(allele_map['present'])
-  #     #print len(important_alleles), len(important_snp_allele), len(unimportant_alleles), len(unimportant_snp_allele)
-  #     if '_' not in snp and '|' not in snp:
-  #       # if snp not in filtered_snps and raw_score == 1:
-  #       #   print "Found defining allele: %s %s %s" % (snp, important_ratio, unimportant_ratio)
-  #       #   new_important_alleles = [allele for allele in important_alleles if allele not in important_snp_allele]
-  #       #   filtered_snps.append(snp)
-  #       #   return [{'allele': important_snp_allele[0], 'snps': filtered_snps}
-  #       if not max_snp or (snp not in filtered_snps and max_snp['score'] < raw_score):
-  #         max_snp = {'snp': snp, 'score': raw_score}
-  #         #print snp, len(allele_map['present'])
-  #         #print len(important_alleles), len(important_snp_allele), len(unimportant_alleles), len(unimportant_snp_allele)
-  #         #print important_ratio, unimportant_ratio
-  #         #print max_snp
-  #       #print "MAX: ", max_snp
-  #   snp_filter.append(max_snp)
-  #   return self.check_alleles(important_alleles, unimportant, snp_filter) + self.check_alleles(remaining_important, unimportant)
-  
-  def check_alleles2(self, important, unimportant, snp_filter=[]):
-    
-    print important, "FILTER", snp_filter
+    logging.debug("In check_alleles.  Important: %s Filter: %s" % (important, snp_filter))
     important_alleles = self.filter_alleles(important, snp_filter)
-    unimportant_alleles = self.filter_alleles(unimportant, snp_filter, False)
+    unimportant_alleles = self.filter_alleles(unimportant, snp_filter, True)
     remaining_important = [allele for allele in important if allele not in important_alleles]
     filtered_snps = [temp_snp['snp'] for temp_snp in snp_filter]
     if not important or len(important_alleles) == 0:
-      return [] 
-    if len(snp_filter) > 1 and len(important_alleles) == 1 and len(unimportant_alleles) == 0:
-      return [{'allele': important_alleles[0], 'snps': filtered_snps}]
+      return []
+    logging.debug("Before recursion check.  snp_filter: %s important %s unimportant %s" % (snp_filter, important_alleles, unimportant_alleles))
+    if len(snp_filter) > 0 and len(important_alleles) == 1 and len(unimportant_alleles) == 0:
+      logging.debug("About to recurse.  important: %s remaining_important %s" % (important_alleles[0], remaining_important))
+      return [{'allele': important_alleles[0], 'snps': filtered_snps}] + self.check_alleles(remaining_important, unimportant, [])
       
     all_alleles = important_alleles + unimportant_alleles
     num_important = len(important_alleles)
-    num_unimportant = len(unimportant_alleles)
-    total = len(all_alleles)
     snp_map = defaultdict(lambda: defaultdict(list))
     for allele in all_alleles:
       seq = self.alleles[allele]
@@ -103,45 +45,35 @@ class AlleleDiff:
     for snp, allele_map in snp_map.iteritems():
       important_snp_allele = [allele for allele in allele_map['present'] if allele in important_alleles]
       raw_score = len(important_snp_allele) / float(num_important)
-      if '_' not in snp and '|' not in snp:
+      if '_' not in snp and '|' not in snp and raw_score > 0:
         all_snp_scores.append({'snp': snp, 'score': raw_score})
         
     sorted_scores = sorted(all_snp_scores, key=itemgetter('score')) 
     for to_test in sorted_scores:
       if to_test['snp'] not in filtered_snps:
         snp_filter.append(to_test)
-        allele_test = self.check_alleles2(important, unimportant, snp_filter)
+        allele_test = self.check_alleles(important, unimportant, snp_filter)
         snp_filter.pop()
         if allele_test:
-          return allele_test + self.check_alleles2(remaining_important, unimportant, [])
+          return allele_test
     return []
   
-  def does_not_contain_alleles(self, allele_combo, test_alleles):
-    return not all(allele in test_alleles for allele in allele_combo)
-    
-  def test_allele_combinations(self, important_allele, seq):
-    for i in range(len(seq)):
-      for allele_combo in combinations(self.important_alleles[important_allele], i):
-        if all(self.does_not_contain_alleles(allele_combo, v) for v in self.unimportant_alleles.values()):
-          return allele_combo
-  
-  def parse_alleles(self, allele_file, wildtype):
-    allele_definitions = {}
-    with open(allele_file) as f:
-      for i, line in enumerate(f):
-        if i < 3:
-          continue
-        allele_def = line.split()
-        allele_string = allele_def[1:]
-        # if allele_def[0] == wildtype:
-        #   allele_string = "".join(allele_string).translate(string.maketrans('ACGT', '____'))
-        #   print allele_string
-        allele_definitions[allele_def[0]] = ["%s:%s" % (i, base) for i, base in enumerate(allele_string)]
-    return allele_definitions
-    
-  def snp_locs(self, allele1, allele2):
-    return ["%s:%s" % (i, allele2[i]) for i, base in enumerate(allele1) if base != allele2[i] ]
-    
+  def filter_alleles(self, alleles, snp_filter, include=True):
+    if len(snp_filter) < 1:
+      return alleles
+    filtered_alleles = []
+    if not include:
+      filtered_alleles = list(alleles)
+    to_filter = [snp['snp'] for snp in snp_filter]
+    for allele in alleles:
+
+      seq = self.alleles[allele]
+      if include and all([snp in seq for snp in to_filter]):
+        filtered_alleles.append(allele)
+      elif not include and not any([snp in seq for snp in to_filter]):
+        filtered_alleles.remove(allele)
+
+    return filtered_alleles
   
 if __name__ == "__main__":
   
--- test_allele_diff.py
+++ test_allele_diff.py
@@ -1,10 +1,13 @@
 import unittest
+import logging
 from allele_diff import AlleleDiff 
 
 class TestAlleleDiff(unittest.TestCase):
   
   def setUp(self):
+    logging.basicConfig(level=logging.DEBUG)
     self.allele_diff = AlleleDiff("test_alleles.txt")
+    self.all_allele_diff = AlleleDiff("HLAB.txt")
     
   def test_filter_alleles_include(self):
     filtered = self.allele_diff.filter_alleles(["B*51240301"], [{'snp': '0:A'}])
@@ -17,28 +20,38 @@ class TestAlleleDiff(unittest.TestCase):
     filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '0:A'}, {'snp': '2:C'}])
     self.assertEqual(1, len(filtered))
     
-  def test_filter_alleles_exclude(self):
-    filtered = self.allele_diff.filter_alleles(["B*51240301"], [{'snp': '0:A'}], False)
-    self.assertEqual(0, len(filtered))
+  def test_filter_alleles(self):
+    filtered = self.allele_diff.filter_alleles(["B*51240301"], [{'snp': '0:A'}])
+    self.assertEqual(1, len(filtered))
     
-    filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '0:A'}], False)
-    self.assertEqual(len(self.allele_diff.alleles.keys()) - 2, len(filtered))
+    filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '0:A'}])
+    self.assertEqual(2, len(filtered))
+    
+    filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '11:G'}])
+    self.assertEqual(2, len(filtered))
     
   def test_filter_alleles_2_exclude(self):
     filtered = self.allele_diff.filter_alleles(self.allele_diff.alleles.keys(), [{'snp': '0:A'}, {'snp': '2:C'}], False)
-    self.assertEqual(len(self.allele_diff.alleles.keys()) - 3, len(filtered))
+    self.assertEqual(4, len(filtered))
     
-  # def test_check_1_allele(self):
-  #   important_alleles = ['B*55070101']
-  #   unimportant_alleles = [allele for allele in self.allele_diff.alleles.keys() if allele not in important_alleles]
-  #   checked = self.allele_diff.check_alleles2(important_alleles, unimportant_alleles)
-  #   print checked
+  def test_check_1_allele(self):
+     important_alleles = ['B*55070101']
+     unimportant_alleles = [allele for allele in self.allele_diff.alleles.keys() if allele not in important_alleles]
+     checked = self.allele_diff.check_alleles(important_alleles, unimportant_alleles)
+     self.assertEqual(2, len(checked[0]['snps']))
     
   def test_check_2_alleles(self):
-    important_alleles = ['B*55070101', 'B*07420101', "B*27090101", 'B*51240301']
+    important_alleles = ['B*55070101', 'B*07420101']
     unimportant_alleles = [allele for allele in self.allele_diff.alleles.keys() if allele not in important_alleles]
-    checked = self.allele_diff.check_alleles2(important_alleles, unimportant_alleles)
-    print checked
+    checked = self.allele_diff.check_alleles(important_alleles, unimportant_alleles)
+    self.assertEqual(2, len(checked))
+    
+    
+  def test_check_4_alleles(self):
+    important_alleles = ['B*51240301', 'B*55070101', 'B*07420101', 'B*27090101']
+    unimportant_alleles = [allele for allele in self.allele_diff.alleles.keys() if allele not in important_alleles]
+    checked = self.allele_diff.check_alleles(important_alleles, unimportant_alleles)
+    self.assertEqual(4, len(checked))    
     
 if __name__ == '__main__':
     unittest.main()
