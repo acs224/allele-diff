e0f4ea33397153061c6ffcd17508d969082aa413
Andrew Shinohara <acs224@georgetown.edu>
2013-05-13 17:34:32 -0400

    Fixing up code and test cases

--- haplo_builder.py
+++ haplo_builder.py
@@ -24,16 +24,13 @@ class HaploBuilder:
     self.NO_CALL = '*'
     self.CALLED_BASES = ['A', 'C', 'G', 'T', '_', '.']
     
-    self.haplos = self.parse_haplos(haplo_file)
-    self.call_cache = defaultdict(lambda: defaultdict(list))
+    self.parse_haplos(haplo_file)
 
   def parse_haplos(self, haplo_file):
     """Parse the haplo_file into a list of POS:BASE combinations.
     
     Args:
       haplo_file: File to parse
-      
-    Returns a dict where haplotype is the key and the value is the list of SNP definitions for that haplotype.
       
     """
     haplo_definitions = {}
@@ -50,7 +47,6 @@ class HaploBuilder:
         allele_list.append(allele_string)
         string_hash = "".join(allele_string)
         if string_hash in self.allele_hash:
-          #print "ERROR: Allele %s is duplicate of %s.  Cannot uniquely identify either allele." % (allele_def[0], self.allele_hash[string_hash])
           self.unidentifiable = self.unidentifiable + self.allele_hash[string_hash]
           self.unidentifiable.append(allele_def[0])
         else:
@@ -58,9 +54,61 @@ class HaploBuilder:
         self.allele_hash[string_hash].append(allele_def[0])
         self.allele_index.append(allele_def[0])
     self.data_matrix = numpy.array(allele_list)
-    return haplo_definitions
- 
-  def check_haplos(self, important, unimportant, snp_filter):
+  
+  def filter_matrix(self, snps):
+    alleles = []
+    locs = []
+    matching_alleles = defaultdict(list)
+    for snp in snps:
+      loc, allele = snp.split(":")
+      alleles.append(allele)
+      locs.append(int(loc))
+    return self.data_matrix[:, locs] == alleles
+  
+  def find_matching(self, snps):
+
+    result = self.filter_matrix(snps)
+    result_all = numpy.all(result, axis=1)
+    index_all = numpy.where(result_all == True)
+
+    matching_alleles = defaultdict(list)
+    for index in index_all[0]:
+      hap_name = self.allele_index[index]
+      compare_results = result[index]
+      for i, r in enumerate(compare_results):
+        if r:
+          matching_alleles[hap_name].append(snps[i])
+    return matching_alleles
+  
+  def filter_haps(self, hap_list, snps):
+
+    result = self.filter_matrix(snps)
+    result_any = numpy.any(result, axis=1)
+    index_any = numpy.where(result_any == True)
+    
+    good_haps = []
+    for index in index_any[0]:
+      hap_name = self.allele_index[index]
+      if hap_name in hap_list:
+        good_haps.append(hap_name)
+    return good_haps
+    
+  def call_haplotype(self, haplotype, unimportant, snp_filter, defining_snps):
+    already_called = [snp for snp_list in defining_snps.values() for snp in snp_list]
+    scored_snps = self.score_snps([haplotype], unimportant, snp_filter, already_called)
+    for snp in scored_snps:
+      new_snp_filter = list(snp_filter)
+      new_snp_filter.append(snp['snp'])
+      call = self.make_call(haplotype, unimportant, new_snp_filter)
+      if call:
+        return call
+      else:
+        results = self.call_haplotype(haplotype, unimportant, new_snp_filter, defining_snps)
+        if results:
+          return results
+    return []
+  
+  def call_haplotypes(self, important, unimportant, snp_filter):
     """Recursively builds a haplotype translation table based upon the important and unimportant haplotypes.
     
     Args:
@@ -72,232 +120,36 @@ class HaploBuilder:
     {haplotype: haplotype name, snps: snps to translate that haplotype}
     
     """
-    data = self.check_haplos2(important, unimportant, snp_filter, {})
-    print data
-    return data
-    
-  def check_haplos2(self, important, unimportant, snp_filter, defining_snps):
     
     identifiable = [hap for hap in important if hap not in self.unidentifiable]
-    if not identifiable:
-      return []
-    scored_snps = self.score_snps2(identifiable, unimportant, snp_filter)
-    
-    brand_new_snp_filter = list(snp_filter)
-    added_snps = False
-    for snp in scored_snps:
-      new_snp_filter = list(snp_filter)
-      new_snp_filter.append(snp['snp'])
-      new = unimportant + defining_snps.keys()
-      call = self.is_callable(identifiable, new, new_snp_filter)
-      if call:
-        print "callable:", call, new_snp_filter
-        for hap, snps in call.iteritems():
-          if hap not in defining_snps:
-            defining_snps[hap] = snps
-        print "DEFINING", defining_snps
-        brand_new_snp_filter.append(snp['snp'])
-        added_snps = True
-          
-    uncallable_haps = [hap for hap in identifiable if hap not in defining_snps.keys()]
-    if not uncallable_haps:
-      return defining_snps
-    else:
-      new = unimportant + defining_snps.keys()
-      scored_snps = self.score_snps2(uncallable_haps, new, snp_filter)
-      print "fitler", brand_new_snp_filter, uncallable_haps, scored_snps[0]
-      if not added_snps:
-        brand_new_snp_filter.append(scored_snps[0]['snp'])
-      return self.check_haplos2(uncallable_haps, unimportant, brand_new_snp_filter, defining_snps)
-    
-  def filter_haps(self, hap_list, snps):
-    alleles = []
-    locs = []
-    for snp in snps:
-      loc, allele = snp.split(":")
-      alleles.append(allele)
-      locs.append(int(loc))
-    result = self.data_matrix[:, locs] == alleles
-    result_any = numpy.any(result, axis=1)
-    result_all = numpy.all(result, axis=1)
-    index_any = numpy.where(result_any == True)
-    index_all = numpy.where(result_all == True)
-    
-    good_haps = []
-    for index in index_any[0]:
-      hap_name = self.allele_index[index]
-      if hap_name in hap_list:
-        good_haps.append(hap_name)
-    return good_haps
-    
-  def call_haplotype(self, haplotype, unimportant, snp_filter, defining_snps):
-    already_called = [snp for snp_list in defining_snps.values() for snp in snp_list]
-    scored_snps = self.score_snps3([haplotype], unimportant, snp_filter, already_called)
-    for snp in scored_snps:
-      new_snp_filter = list(snp_filter)
-      new_snp_filter.append(snp['snp'])
-      call = self.is_callable2([haplotype], unimportant, new_snp_filter)
-      if call:
-        return call
-      else:
-        results = self.call_haplotype(haplotype, unimportant, new_snp_filter, defining_snps)
-        if results:
-          return results
-    return []
-  
-  def call_haplotypes(self, important, unimportant, snp_filter):
-    identifiable = [hap for hap in important if hap not in self.unidentifiable]
+    unidentifiable = [hap for hap in important if hap not in identifiable]
     if not identifiable:
       return []
     all_haps = {}
     for haplotype in identifiable:
-      new = unimportant + [hap for hap in identifiable if hap != haplotype]
-      result = self.call_haplotype(haplotype, new, snp_filter, all_haps)
+      others = unimportant + [hap for hap in identifiable if hap != haplotype]
+      result = self.call_haplotype(haplotype, others, snp_filter, all_haps)
       all_haps.update(result)
+      
+    for hap in unidentifiable:
+      all_haps[hap] = ['Unidentifiable']
     return all_haps
 
-  def is_callable(self, important, unimportant, snps):
-    matching_alleles = self.find_matching(important, unimportant, snps)
+  def make_call(self, haplotype, unimportant, snps):
+    matching_alleles = self.find_matching(snps)
     snp_map = defaultdict(list)
     good_snp_map = {}
-    #print snps, matching_alleles
-    for haplotype, matching_snps in matching_alleles.iteritems():
-      for i in range(1, len(matching_snps) + 1):
-        for combo in itertools.combinations(matching_snps, i):
-          snp_hash = "|".join(combo)
-          snp_map[snp_hash].append(haplotype)
-    #print snp_map
-    for snp_hash, haps in snp_map.iteritems():
-      if len(haps) == 1 and haps[0] in important:
-        good_snp_map[haps[0]] = snp_hash.split("|")
-    #print "MAP:",  snp_map, good_snp_map
-    #print snps, len(matching_alleles), matching_alleles
-    if good_snp_map:
-      #print "MATCHING", matching_alleles
-      #print "snp_map", snp_map
-      #print "returning",  good_snp_map
-      return good_snp_map
-    return False
-    
-  def is_callable2(self, important, unimportant, snps):
-    matching_alleles = self.find_matching2(important, unimportant, snps)
-    snp_map = defaultdict(list)
-    good_snp_map = {}
-    # for haplotype, matching_snps in matching_alleles.iteritems():
-    #   for i in range(1, len(matching_snps) + 1):
-    #     for combo in itertools.combinations(matching_snps, i):
-    #       snp_hash = "|".join(combo)
-    #       snp_map[snp_hash].append(haplotype)
-          
     for haplotype, matching_snps in matching_alleles.iteritems():
       snp_hash = "|".join(matching_snps)
       snp_map[snp_hash].append(haplotype)
-    #print snp_map
     for snp_hash, haps in snp_map.iteritems():
-      if len(haps) == 1 and haps[0] in important:
+      if len(haps) == 1 and haps[0] == haplotype:
         good_snp_map[haps[0]] = snp_hash.split("|")
-    #print important, good_snp_map
     if good_snp_map:
       return good_snp_map
     return False
     
-  def print_matrix(self,  sample_index, matrix):
-    for i, row in enumerate(matrix):
-      print self.allele_index[sample_index[i]], row
-  
-  def find_matching(self, important, unimportant, snps):
-    
-    alleles = []
-    locs = []
-    matching_alleles = defaultdict(list)
-    for snp in snps:
-      loc, allele = snp.split(":")
-      alleles.append(allele)
-      locs.append(int(loc))
-    result = self.data_matrix[:, locs] == alleles
-    result_any = numpy.any(result, axis=1)
-    result_all = numpy.all(result, axis=1)
-    index_any = numpy.where(result_any == True)
-    index_all = numpy.where(result_all == True)
-    
-    for index in index_any[0]:
-      hap_name = self.allele_index[index]
-      compare_results = result[index]
-      for i, r in enumerate(compare_results):
-        if r:
-          matching_alleles[hap_name].append(snps[i])
-    return matching_alleles
-    
-  def find_matching2(self, important, unimportant, snps):
-
-    alleles = []
-    locs = []
-    matching_alleles = defaultdict(list)
-    for snp in snps:
-      loc, allele = snp.split(":")
-      alleles.append(allele)
-      locs.append(int(loc))
-    result = self.data_matrix[:, locs] == alleles
-    result_any = numpy.any(result, axis=1)
-    result_all = numpy.all(result, axis=1)
-    index_any = numpy.where(result_any == True)
-    index_all = numpy.where(result_all == True)
-
-    for index in index_all[0]:
-      hap_name = self.allele_index[index]
-      compare_results = result[index]
-      for i, r in enumerate(compare_results):
-        if r:
-          matching_alleles[hap_name].append(snps[i])
-    return matching_alleles
-    
-  def score_snps2(self, important_haplos, unimportant_haplos, snp_filter):
-    """Assign a score to each snp.  This score is meant to minimize the number
-    of snps that should be used to define a haplotype.  The current formula is:
-    
-    score = (ratio of snp in important haplos + .5) / (ratio of snp in unimportant haplos + .5) + (weighted score to favor snps over reference)
-    Note: .5 is added to each ratio in case the denominator is 0
-    
-    Args:
-      important_haplos: List of important haplotypes.
-      unimportant_haplos: List of unimportant haplotypes.
-      snp_map: Dictionary of all snps.
-      
-    Returns a list of scored snps. (Higher score is better, has more chance of having useful information)
-    """
-    all_snp_scores = []
-    unimportant_filtered = unimportant_haplos
-    important_filtered = important_haplos
-    if snp_filter:
-      print "FILTER", snp_filter
-      unimportant_filtered = self.filter_haps(unimportant_haplos, snp_filter)
-      #important_filtered = self.filter_haps(important_haplos, snp_filter)
-    important_indexes = [i for i, item in enumerate(self.allele_index) if item in important_filtered]
-    unimportant_indexes = [i for i, item in enumerate(self.allele_index) if item in unimportant_filtered]
-    
-    for i, col in enumerate(self.data_matrix.T):
-      important_counter = Counter(col[important_indexes,:])
-      unimportant_counter = Counter(col[unimportant_indexes,:])
-      for base in self.CALLED_BASES:
-        if base not in important_counter and base not in unimportant_counter:
-          continue
-        snp = "%s:%s" % (i, base)
-        if snp in snp_filter:
-          continue
-        important_ratio = 0
-        if len(important_indexes) > 0:
-          important_ratio = important_counter[base] / float(len(important_indexes))
-        unimportant_ratio = 0
-        if len(unimportant_indexes) > 0:
-          unimportant_ratio = unimportant_counter[base] / float(len(unimportant_filtered))
-        if important_ratio > 0:
-          raw_score = (1 - important_ratio) + (1 - unimportant_ratio) * (.9 if "_" in snp else 1)
-          if raw_score > 0:
-            all_snp_scores.append({'snp': snp, 'score': raw_score, 'import_ratio': important_ratio, 'unimport_ratio': unimportant_ratio})
-    
-    return sorted(all_snp_scores, key=itemgetter('score'), reverse=True)
-    
-  def score_snps3(self, important_haplos, unimportant_haplos, snp_filter, used_snps):
+  def score_snps(self, important_haplos, unimportant_haplos, snp_filter, used_snps):
     """Assign a score to each snp.  This score is meant to minimize the number
     of snps that should be used to define a haplotype.  The current formula is:
 
@@ -331,13 +183,11 @@ class HaploBuilder:
         important_ratio = important_counter[base] / float(len(important_indexes))
         unimportant_ratio = unimportant_counter[base] / float(len(unimportant_filtered))
         if important_ratio > 0:
-          raw_score = (1 - important_ratio) + (1 - unimportant_ratio) * (.9 if "_" in snp else 1) #* (1.1 if snp in used_snps else 1)
+          raw_score = (1 - important_ratio) + (1 - unimportant_ratio) * (.9 if "_" in snp else 1)
           if raw_score > 0:
-            all_snp_scores.append({'snp': snp, 'score': raw_score, 'import_ratio': important_ratio, 'unimport_ratio': unimportant_ratio})
+            all_snp_scores.append({'snp': snp, 'score': raw_score})
 
     return sorted(all_snp_scores, key=itemgetter('score'), reverse=True)
-  
-    
   
 if __name__ == "__main__":
   parser = OptionParser()
@@ -356,9 +206,3 @@ if __name__ == "__main__":
   haplo_mapping = haplo_builder.call_haplotypes(important_haplos, unimportant_haplos, [])
   for item, snps in haplo_mapping.iteritems():
     print "%s\t%s" % (item, "\t".join(sorted(snps)))
-  # 
-  # important_haplos = ['B*55070101']
-  # all_haplo_diff = HaploBuilder("HLAB.txt")
-  # unimportant_haplos = [hap for hap in all_haplo_diff.haplos.keys() if hap not in important_haplos]
-  # checked = all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
-  # print checked
--- test_haplo_builder.py
+++ test_haplo_builder.py
@@ -1,6 +1,7 @@
 import unittest
 import logging
 from haplo_builder import HaploBuilder
+from numpy import logical_and
 
 class TestHaploBuilder(unittest.TestCase):
   
@@ -9,78 +10,93 @@ class TestHaploBuilder(unittest.TestCase):
     self.haplo_diff = HaploBuilder("test_haplos.txt")
     self.all_haplo_diff = HaploBuilder("HLAB.txt")
     
-  def test_filter_haplos_include(self):
-    filtered = self.haplo_diff.filter_haplos(["B*51240301"], [{'snp': '0:A'}])
-    self.assertEqual(1, len(filtered))
-    
-    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '0:A'}])
-    self.assertEqual(2, len(filtered))
-    
-  def test_filter_haplos_2_include(self):
-    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '0:A'}, {'snp': '2:C'}])
-    self.assertEqual(1, len(filtered))
-    
   def test_filter_haplos(self):
-    filtered = self.haplo_diff.filter_haplos(["B*51240301"], [{'snp': '0:A'}])
+    filtered = self.haplo_diff.filter_haps(["B*51240301"], ['0:A'])
     self.assertEqual(1, len(filtered))
     
-    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '0:A'}])
+    filtered = self.haplo_diff.filter_haps(self.haplo_diff.allele_index, ['0:A'])
     self.assertEqual(2, len(filtered))
     
-    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '11:G'}])
-    self.assertEqual(2, len(filtered))
-    
-  def test_filter_haplos_2_exclude(self):
-    filtered = self.haplo_diff.filter_haplos(self.haplo_diff.haplos.keys(), [{'snp': '0:A'}, {'snp': '2:C'}], False)
+  def test_filter_haplos_2(self):
+    filtered = self.haplo_diff.filter_haps(self.haplo_diff.allele_index, ['0:A','2:C'])
     self.assertEqual(4, len(filtered))
     
-  def test_check_1_hap(self):
-     important_haplos = ['B*55070101']
-     unimportant_haplos = [hap for hap in self.haplo_diff.haplos.keys() if hap not in important_haplos]
-     checked = self.haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
-     self.assertEqual(2, len(checked[0]['snps']))
+  def test_is_callable(self):
+    haplo = 'B*51240301'
+    others = [hap for hap in self.haplo_diff.allele_index if hap != haplo]
+    result = self.haplo_diff.make_call(haplo, others, ['0:A'])
+    self.assertFalse(result)
     
+    haplo = 'B*51240301'
+    others = [hap for hap in self.haplo_diff.allele_index if hap != haplo]
+    result = self.haplo_diff.make_call(haplo, others, ['0:A', '11:_'])
+    self.assertEqual(1, len(result))
+  
+  def test_check_1_hap(self):
+     important_haplos = ['B*07420101']
+     unimportant_haplos = [hap for hap in self.haplo_diff.allele_index if hap not in important_haplos]
+     checked = self.haplo_diff.call_haplotypes(important_haplos, unimportant_haplos, [])
+     self.assertEqual(1, len(checked[important_haplos[0]]))
+     self.assertTrue('3:G' in checked[important_haplos[0]])
+  
+  def test_filter(self):
+    haps = ['B*51240301', 'B*07420101', 'B*27090101', 'B*55070101', 'B*35700101']
+    snps = ['2:C']
+    checked = self.haplo_diff.filter_haps(haps, snps)
+    self.assertEqual(3, len(checked))
+    
+    haps = ['B*51240301', 'B*07420101', 'B*27090101', 'B*55070101', 'B*35700101']
+    snps = ['2:C', '0:A']
+    checked = self.haplo_diff.filter_haps(haps, snps)
+    self.assertEqual(4, len(checked))
+    
+    haps = ['B*15020101', 'B*35700101', 'B*51560101', 'B*07760101', 'B*50060101', 'B*08340101', 'B*52060201', 'B*08130101']
+    snps = ['3:G', '4:A', '2:C']
+    checked = self.haplo_diff.filter_haps(haps, snps)
+    self.assertEqual(0, len(checked))
+  
   def test_check_2_haplos(self):
-    important_haplos = ['B*55070101', 'B*07420101']
-    unimportant_haplos = [hap for hap in self.haplo_diff.haplos.keys() if hap not in important_haplos]
-    checked = self.haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+    important_haplos = ['B*51240301', 'B*55070101']
+    unimportant_haplos = [hap for hap in self.haplo_diff.allele_index if hap not in important_haplos]
+    checked = self.haplo_diff.call_haplotypes(important_haplos, unimportant_haplos, [])
     self.assertEqual(2, len(checked))
     
   def test_check_4_haplos(self):
     important_haplos = ['B*51240301', 'B*55070101', 'B*07420101', 'B*27090101']
-    unimportant_haplos = [hap for hap in self.haplo_diff.haplos.keys() if hap not in important_haplos]
-    checked = self.haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+    unimportant_haplos = [hap for hap in self.haplo_diff.allele_index if hap not in important_haplos]
+    checked = self.haplo_diff.call_haplotypes(important_haplos, unimportant_haplos, [])
     self.assertEqual(4, len(checked))    
     
+  def test_check_hard_haplos(self):
+     important_haplos = ['B*51240301', 'B*55070101', 'B*35700101']
+     unimportant_haplos = [hap for hap in self.all_haplo_diff.allele_index if hap not in important_haplos]
+     checked = self.all_haplo_diff.call_haplotypes(important_haplos, unimportant_haplos, [])
+     self.assertEqual(3, len(checked))  
+     self.assertEqual(3, len(checked['B*51240301']))  
+     self.assertEqual(4, len(checked['B*55070101']))  
+     self.assertTrue('Unidentifiable' in checked['B*35700101'])
+    
   def test_all_check_1(self):
-    important_haplos = ['B*55070101']
-    #important_haplos = ['B*51240301']
-    unimportant_haplos = [hap for hap in self.all_haplo_diff.haplos.keys() if hap not in important_haplos]
-    checked = self.all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
+    # This seems to be the most difficult haplotype to call in the set
+    important_haplos = ['B*51090201']
+    unimportant_haplos = [hap for hap in self.all_haplo_diff.allele_index if hap not in important_haplos]
+    checked = self.all_haplo_diff.call_haplotypes(important_haplos, unimportant_haplos, [])
     self.assertEqual(1, len(checked))   
+    self.assertEqual(61, len(checked['B*51090201']))   
         
-  def test_all_check_4(self):
-    important_haplos = ['B*51240301', 'B*55070101', 'B*07420101', 'B*27090101']
-    unimportant_haplos = [hap for hap in self.all_haplo_diff.haplos.keys() if hap not in important_haplos]
-    checked = self.all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
-    self.assertEqual(4, len(checked))
+  def test_all_check_2(self):
+     important_haplos = ['B*35700101', 'B*52060201']
+     unimportant_haplos = [hap for hap in self.all_haplo_diff.allele_index if hap not in important_haplos]
+     checked = self.all_haplo_diff.call_haplotypes(important_haplos, unimportant_haplos, [])
+     self.assertEqual(2, len(checked))
   
-  # def test_all_check_10(self):
-  #   important_haplos = ['B*51240301', 'B*55070101', 'B*35700101', 'B*07420101', 'B*27090101', 'B*51560101', 'B*07760101', 'B*50060101', 'B*08340101', 'B*52060201']
-  #   unimportant_haplos = [hap for hap in self.all_haplo_diff.haplos.keys() if hap not in important_haplos]
-  #   checked = self.all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
-  #   self.assertEqual(10, len(checked))
-    
-  # def test_all_check(self):
-  #   for important_hap in self.all_haplo_diff.haplos.keys():
-  #     important_haplos = [important_hap]
-  #     unimportant_haplos = [hap for hap in self.all_haplo_diff.haplos.keys() if hap not in important_haplos]
-  #     checked = self.all_haplo_diff.check_haplos(important_haplos, unimportant_haplos, [])
-  #     print checked
-  #     if len(checked) != 1:
-  #       print "ERROR", important_haplos, checked
-  #     else:
-  #       print "no error", important_haplos
-    
+  def test_all_check_10(self):
+    important_haplos = ['B*51240301', 'B*55070101', 'B*35700101', 'B*07420101', 'B*27090101', 'B*51560101', 'B*07760101', 'B*50060101', 'B*08340101', 'B*08130101']
+    unimportant_haplos = [hap for hap in self.all_haplo_diff.allele_index if hap not in important_haplos]
+    checked = self.all_haplo_diff.call_haplotypes(important_haplos, unimportant_haplos, [])
+    called_snps = [snp for snp_list in checked.values() for snp in snp_list]
+    self.assertEqual(10, len(checked))
+    self.assertEqual(24, len(called_snps))
+  
 if __name__ == '__main__':
     unittest.main()
